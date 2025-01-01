2c862e14d6e74affa9f2f5f7d5ed73ed8e60d973
doanh <nguyentrongdoanh202@gmail.com>
2025-01-01 11:32:43 +0700

    config kafka #1

--- identity-service/src/main/java/com/ndtdoanh/identity/service/UserService.java
+++ identity-service/src/main/java/com/ndtdoanh/identity/service/UserService.java
@@ -3,6 +3,7 @@ package com.ndtdoanh.identity.service;
 import java.util.HashSet;
 import java.util.List;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,6 +39,7 @@ public class UserService {
     PasswordEncoder passwordEncoder;
     ProfileClient profileClient;
     ProfileMapper profileMapper;
+    KafkaTemplate<String, String> kafkaTemplate;
 
     public UserResponse createUser(UserRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
@@ -55,6 +57,9 @@ public class UserService {
         profileRequest.setUserId(user.getId());
 
         profileClient.createProfile(profileRequest);
+
+        //publish message to kafka
+        kafkaTemplate.send("onboard-successful", "welcome our new member " + user.getUsername());
 
         return userMapper.toUserResponse(user);
     }
--- notification-service/src/main/java/com/ndtdoanh/notification/controller/EmailController.java
+++ notification-service/src/main/java/com/ndtdoanh/notification/controller/EmailController.java
@@ -1,20 +1,21 @@
 package com.ndtdoanh.notification.controller;
 
 import com.ndtdoanh.notification.dto.ApiResponse;
-import com.ndtdoanh.notification.dto.request.EmailRequest;
 import com.ndtdoanh.notification.dto.request.SendEmailRequest;
 import com.ndtdoanh.notification.dto.response.EmailResponse;
 import com.ndtdoanh.notification.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EmailController {
     EmailService emailService;
@@ -24,5 +25,10 @@ public class EmailController {
         return ApiResponse.<EmailResponse>builder()
                 .result(emailService.sendEmail(request))
                 .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message) {
+        log.info("message received: {}", message);
     }
 }
